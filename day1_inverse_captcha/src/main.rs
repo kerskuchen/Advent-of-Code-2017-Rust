49c457d0b884a338f45d8bdebd230f411872c1a3
Jakob Schwab <j.schwab@mail.de>
2018-06-13 10:04:03 +0200

    Adds second part to day 1

--- day1_inverse_captcha/src/main.rs
+++ day1_inverse_captcha/src/main.rs
@@ -2,28 +2,28 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let mut input_string = String::new();
+    let mut input = String::new();
     File::open("input.txt")
         .unwrap()
-        .read_to_string(&mut input_string)
+        .read_to_string(&mut input)
         .unwrap();
-    input_string.trim();
 
-    let mut input_digits = Vec::new();
-    for character in input_string.chars() {
-        if let Some(digit) = character.to_digit(10) {
-            input_digits.push(digit);
-        }
-    }
+    let digits: Vec<u32> = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
 
+    println!("Part 1: {}", captcha_solution(1, &digits));
+    println!("Part 2: {}", captcha_solution(digits.len() / 2, &digits));
+}
+
+fn captcha_solution(step_distance: usize, digits: &[u32]) -> u32 {
     let mut sum = 0;
-    let mut last_digit = input_digits[input_digits.len() - 1];
-    for digit in input_digits {
-        if digit == last_digit {
-            sum += last_digit;
+    for index in 0..digits.len() {
+        if digits[index] == digits[(index + step_distance) % digits.len()] {
+            sum += digits[index];
         }
-        last_digit = digit;
     }
-
-    println!("{}", sum);
+    sum
 }
