8128029bcea4f6d737d53640b433c7b1aa133647
Jakob Schwab <j.schwab@mail.de>
2018-06-16 12:13:54 +0200

    Adds second part to day 4

--- day4_high_entropy_passphrases/src/main.rs
+++ day4_high_entropy_passphrases/src/main.rs
@@ -1,27 +1,39 @@
-use std::collections::HashSet;
+extern crate itertools;
+use itertools::Itertools;
+
 use std::fs::File;
-use std::io::Read;
+use std::io::*;
 
 fn main() {
-    let mut input = String::new();
-    File::open("input.txt")
-        .unwrap()
-        .read_to_string(&mut input)
-        .unwrap();
+    let lines: Vec<Vec<String>> = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            line.trim()
+                .split_whitespace()
+                .map(|word| word.to_string())
+                .collect()
+        })
+        .collect();
 
-    let mut num_valids = 0;
-    'lines: for line in input.lines() {
-        let words: Vec<_> = line.trim().split_whitespace().collect();
-        let mut map = HashSet::new();
+    println!(
+        "Number of valid passphrases: {}",
+        // Count the lines where the number of unique words is the number of total words
+        lines
+            .iter()
+            .filter(|line| line.iter().unique().count() == line.len())
+            .count()
+    );
 
-        for word in &words {
-            let cloned = word.clone();
-            if map.get(cloned).is_some() {
-                continue 'lines;
-            }
-            map.insert(cloned);
-        }
-        num_valids += 1;
-    }
-    println!("{}", num_valids);
+    println!(
+        "Number of valid anagram passphrases: {:?}",
+        // Count the lines where the number of unique sorted words is the number of total words
+        lines
+            .iter()
+            .filter(|line| line.iter()
+                .map(|word| word.chars().sorted())
+                .unique()
+                .count() == line.len())
+            .count()
+    );
 }
