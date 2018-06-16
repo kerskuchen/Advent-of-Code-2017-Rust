cafff00c4aafd428ce52967b84324c4b6e6c8ac3
Jakob Schwab <j.schwab@mail.de>
2018-06-16 13:01:42 +0200

    Adds second part to day 5

--- day5_a_maze_of_twisty_trampolines_all_alike/src/main.rs
+++ day5_a_maze_of_twisty_trampolines_all_alike/src/main.rs
@@ -1,23 +1,35 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::*;
 
 fn main() {
-    let mut input = String::new();
-    File::open("input.txt")
-        .unwrap()
-        .read_to_string(&mut input)
-        .unwrap();
+    let jump_offsets: Vec<i32> = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-    let mut instructions: Vec<i32> = input.lines().map(|x| x.parse::<i32>().unwrap()).collect();
+    let jump_increment_function_part1 = |x| x + 1;
+    println!(
+        "Number of jumps for part 1: {}",
+        do_jumping(&jump_offsets, jump_increment_function_part1)
+    );
 
+    let jump_increment_function_part2 = |x| if x >= 3 { x - 1 } else { x + 1 };
+    println!(
+        "Number of jumps for part 2: {}",
+        do_jumping(&jump_offsets, jump_increment_function_part2)
+    );
+}
+
+fn do_jumping(jump_offsets: &[i32], jump_increment: fn(i32) -> i32) -> usize {
+    let mut jump_offsets: Vec<_> = jump_offsets.to_vec();
     let mut num_jumps = 0;
-    let mut pos = 0i32;
-    while (pos >= 0) && ((pos as usize) < instructions.len()) {
-        let old_pos = pos;
-        pos += instructions[pos as usize];
-        instructions[old_pos as usize] += 1;
+    let mut cur_pos = 0i32;
+    while (cur_pos >= 0) && ((cur_pos as usize) < jump_offsets.len()) {
+        let old_pos = cur_pos;
+        cur_pos += jump_offsets[cur_pos as usize];
+        jump_offsets[old_pos as usize] = jump_increment(jump_offsets[old_pos as usize]);
         num_jumps += 1;
     }
-
-    println!("{:?}", num_jumps);
+    num_jumps
 }
