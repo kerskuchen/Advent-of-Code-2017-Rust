676280481576484b629a26e49f7b15959f54e235
Jakob Schwab <j.schwab@mail.de>
2018-06-28 18:34:21 +0200

    Adds second part to day 7

--- day7_recursive_circus/src/main.rs
+++ day7_recursive_circus/src/main.rs
@@ -1,37 +1,175 @@
-use std::fs::File;
-use std::io::Read;
+extern crate itertools;
+use itertools::Itertools;
 
-use std::collections::HashSet;
+use std::fs::File;
+use std::io::*;
+
+use std::collections::HashMap;
+
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Disc {
+    name: String,
+    weight: i32,
+    parent: RefCell<Weak<Disc>>,
+    children: RefCell<Vec<Rc<Disc>>>,
+}
 
 fn main() {
-    let mut input_string = String::new();
-    File::open("input.txt")
-        .unwrap()
-        .read_to_string(&mut input_string)
-        .unwrap();
+    // Parse input
+    let entries: Vec<Vec<String>> = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|maybe_line| maybe_line.ok())
+        .map(|line| {
+            line.replace("(", " ")
+                .replace(")", " ")
+                .replace("->", " ")
+                .replace(",", " ")
+                .split_whitespace()
+                .map(|part| part.to_string())
+                .collect()
+        })
+        .collect();
 
-    let mut list_of_nodes_with_children = HashSet::new();
-    let mut list_of_children = HashSet::new();
-    for line in input_string.lines() {
-        // NOTE: We ignore the weight as it is not needed for this problem
-        let names: String = line.chars()
-            .map(|x| if x.is_ascii_alphabetic() { x } else { ' ' })
-            .collect();
-        let names: Vec<_> = names.split_whitespace().collect();
+    // Create discs
+    let discs: HashMap<String, Rc<Disc>> = entries
+        .iter()
+        .map(|entry| {
+            let name = entry[0].clone();
+            let disc = Rc::new(Disc {
+                name: name.clone(),
+                weight: entry[1].parse().unwrap(),
+                parent: RefCell::new(Weak::new()),
+                children: RefCell::new(Vec::new()),
+            });
+            (name, disc)
+        })
+        .collect();
 
-        if names.len() > 1 {
-            // Collect all nodes that have children
-            list_of_nodes_with_children.insert(String::from(names[0]));
-            for &name in names.iter().skip(1) {
-                list_of_children.insert(String::from(name));
-            }
-        } else {
-            // Leaf nodes count as children
-            list_of_children.insert(String::from(names[0]));
+    // Assign children to discs
+    for entry in entries.iter().filter(|entry| entry.len() > 2) {
+        let parent_name = &entry[0];
+        let mut parent = &discs[parent_name];
+
+        let children_names: Vec<_> = entry.iter().skip(2).collect();
+        for child_name in children_names {
+            let mut child = &discs[child_name];
+            *child.parent.borrow_mut() = Rc::downgrade(&parent);
+            parent.children.borrow_mut().push(Rc::clone(&child));
         }
     }
 
-    // The root node is the only node with children which has no parent
-    let root_node = list_of_nodes_with_children.difference(&list_of_children);
-    println!("{:?}", root_node);
+    // Determine root disc
+    // NOTE: The root disc is the only disc without a parent
+    let mut root_name = None;
+    for disc in discs.values() {
+        if disc.parent.borrow().upgrade().is_none() {
+            root_name = Some(disc.name.clone());
+            break;
+        }
+    }
+    let root_disc = discs[&root_name.unwrap()].clone();
+    println!("Root disc name: '{}'", root_disc.name);
+
+    // Determine which one of the discs is unbalanced
+    let last_unbalanced_disk = find_last_unbalanced_disk_recursive(root_disc);
+    let children_tree_weights: Vec<i32> = last_unbalanced_disk
+        .children
+        .borrow()
+        .iter()
+        .map(|child| calc_disc_tree_weight_recursive(child))
+        .collect();
+    println!(
+        "The last unbalanced discs name is: '{}'",
+        last_unbalanced_disk.name
+    );
+    println!(
+        "The child-trees of '{}' have the following weights:",
+        last_unbalanced_disk.name
+    );
+    println!(
+        "{:?}",
+        last_unbalanced_disk
+            .children
+            .borrow()
+            .iter()
+            .map(|child| child.name.clone())
+            .zip(children_tree_weights.iter())
+            .collect::<Vec<_>>()
+    );
+
+    // Determine what the correct weight of each child-tree should be
+    let correct_child_tree_weight = if (children_tree_weights[0] == children_tree_weights[1])
+        || (children_tree_weights.len() == 2)
+        || (children_tree_weights.len() > 2
+            && (children_tree_weights[0] == children_tree_weights[2]))
+    {
+        children_tree_weights[0]
+    } else {
+        children_tree_weights[1]
+    };
+
+    // Determine which child needs to change its weight
+    let (index_of_wrong_child_tree_weight, wrong_child_tree_weight) = children_tree_weights
+        .iter()
+        .enumerate()
+        .find(|(_index, &tree_weight)| tree_weight != correct_child_tree_weight)
+        .unwrap();
+
+    let child_with_wrong_weight =
+        &last_unbalanced_disk.children.borrow()[index_of_wrong_child_tree_weight];
+    let child_name = &child_with_wrong_weight.name;
+    let wrong_weight = child_with_wrong_weight.weight;
+    println!(
+        "The disc causing unbalance is called '{}' and has a weight of {}",
+        child_name, wrong_weight
+    );
+
+    let correct_child_weight = correct_child_tree_weight - (wrong_child_tree_weight - wrong_weight);
+    println!(
+        "The correct weight for '{}' needs to be {}",
+        child_name, correct_child_weight
+    );
+}
+
+/// Descents down a disc tree until it finds the disc `a` for wich holds:
+/// 1) Every child of `a` is balanced
+/// 2) 'a' itself is not balanced
+///
+/// NOTE: A disc tree is balanced if all of its child-trees have the same weight
+///
+fn find_last_unbalanced_disk_recursive(unbalanced_disk: Rc<Disc>) -> Rc<Disc> {
+    let maybe_unbalanced_child = unbalanced_disk
+        .children
+        .borrow()
+        .iter()
+        .cloned()
+        .find(|child| !is_disc_tree_balanced(child));
+
+    match maybe_unbalanced_child {
+        Some(child) => find_last_unbalanced_disk_recursive(child.clone()),
+        // If all of the disc's children are balanced it means the disc itself must be unbalanced
+        None => unbalanced_disk,
+    }
+}
+
+/// Returns true if all child-trees of `disc` have the same weight
+fn is_disc_tree_balanced(disc: &Rc<Disc>) -> bool {
+    let children_weights: Vec<i32> = disc.children
+        .borrow()
+        .iter()
+        .map(|child| calc_disc_tree_weight_recursive(child))
+        .sorted();
+    (children_weights[0] == children_weights[children_weights.len() - 1])
+}
+
+/// Returns the weight of the whole disc-tree
+fn calc_disc_tree_weight_recursive(disc: &Rc<Disc>) -> i32 {
+    let mut weight = disc.weight;
+    for child in disc.children.borrow().iter() {
+        weight += calc_disc_tree_weight_recursive(child);
+    }
+    weight
 }
