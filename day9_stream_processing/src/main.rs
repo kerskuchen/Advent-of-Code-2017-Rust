9992561070ace0a4d05fecdb47017e5ddea68e5a
Jakob Schwab <j.schwab@mail.de>
2018-06-30 11:11:03 +0200

    Adds second part to day 9

--- day9_stream_processing/src/main.rs
+++ day9_stream_processing/src/main.rs
@@ -14,25 +14,26 @@ fn main() {
         .read_to_string(&mut input)
         .unwrap();
 
-    let mut group_nesting_score = 0;
     let mut total_score = 0;
+    let mut group_nesting_level = 0;
+    let mut garbage_char_count = 0;
     let mut parse_mode = ParseMode::Group;
 
     for c in input.chars() {
         match parse_mode {
             ParseMode::Group => match c {
                 '{' => {
-                    group_nesting_score += 1;
-                    total_score += group_nesting_score;
+                    group_nesting_level += 1;
+                    total_score += group_nesting_level;
                 }
-                '}' => group_nesting_score -= 1,
+                '}' => group_nesting_level -= 1,
                 '<' => parse_mode = ParseMode::Garbage,
                 _ => {}
             },
             ParseMode::Garbage => match c {
                 '!' => parse_mode = ParseMode::Ignore,
                 '>' => parse_mode = ParseMode::Group,
-                _ => {}
+                _ => garbage_char_count += 1,
             },
             ParseMode::Ignore => {
                 // NOTE: '!' symbols can only exist in garbage so we can safely switch back to
@@ -42,5 +43,6 @@ fn main() {
         }
     }
 
-    println!("{}", total_score);
+    println!("Total score: {}", total_score);
+    println!("Garbage characters count: {}", garbage_char_count);
 }
