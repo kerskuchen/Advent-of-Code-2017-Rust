15f2d4fc208357cca79b694761f96b0b3a61be30
Jakob Schwab <j.schwab@mail.de>
2018-07-02 13:12:24 +0200

    Adds second part to day 12

--- day12_digital_plumber/src/main.rs
+++ day12_digital_plumber/src/main.rs
@@ -1,27 +1,48 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::File;
-use std::io::Read;
+use std::io::*;
 
 fn main() {
-    let mut input = String::new();
-    File::open("input.txt")
-        .unwrap()
-        .read_to_string(&mut input)
-        .unwrap();
+    let lines: Vec<String> = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|maybe_line| maybe_line.ok())
+        .map(|line| line.replace(" <->", ","))
+        .collect();
 
+    // Create program realations
     let mut relations = HashMap::new();
-    let input = input.replace(" <->", ",");
-    for line in input.lines() {
+    for line in lines {
         let ids: Vec<u32> = line.split(',').map(|x| x.trim().parse().unwrap()).collect();
         let main_id = ids[0];
         let related_ids: Vec<u32> = ids.into_iter().skip(1).collect();
         relations.insert(main_id, related_ids);
     }
-    println!("{:?}", elems_in_group_of_id(0, &relations).len());
+
+    // Determine number of groups
+    let mut num_groups_found = 0;
+    let mut programs_not_in_a_group: HashSet<u32> = (0..2000).collect();
+    for program_id in 0..2000 {
+        let group = group_of_id(program_id, &relations);
+        if group.is_subset(&programs_not_in_a_group) {
+            num_groups_found += 1;
+            programs_not_in_a_group = programs_not_in_a_group
+                .difference(&group)
+                .cloned()
+                .collect();
+        }
+    }
+    assert!(programs_not_in_a_group.is_empty());
+
+    println!(
+        "Number of programs in group that contain program ID 0: {}",
+        group_of_id(0, &relations).len()
+    );
+    println!("Number of unique groups: {}", num_groups_found);
 }
 
-fn elems_in_group_of_id(id: u32, relations: &HashMap<u32, Vec<u32>>) -> HashSet<u32> {
+/// Returns a set of all elements that are in the same group as the program with a given ID
+fn group_of_id(id: u32, relations: &HashMap<u32, Vec<u32>>) -> HashSet<u32> {
     let mut group = HashSet::new();
     let mut todo_stack = vec![id];
     while !todo_stack.is_empty() {
