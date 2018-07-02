534f8e8206376b2acb80615a783d6edbb70d43a9
Jakob Schwab <j.schwab@mail.de>
2018-07-02 17:57:06 +0200

    Adds second part to day 13

--- day13_packet_scanners/src/main.rs
+++ day13_packet_scanners/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::*;
 
+#[derive(Clone, Debug)]
 struct Layer {
     range: i32,
     scanner_pos: i32,
@@ -8,15 +9,14 @@ struct Layer {
 }
 
 fn main() {
-    let mut input = String::new();
-    File::open("input.txt")
-        .unwrap()
-        .read_to_string(&mut input)
-        .unwrap();
+    let lines: Vec<String> = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|maybe_line| maybe_line.ok())
+        .collect();
 
     // Populate layers
     let mut firewall = Vec::new();
-    for line in input.lines() {
+    for line in lines {
         let layer_info: Vec<i32> = line.split(':').map(|x| x.trim().parse().unwrap()).collect();
         let depth = layer_info[0];
         let range = layer_info[1];
@@ -35,24 +35,55 @@ fn main() {
         firewall.push(Some(layer));
     }
 
-    let mut severity = 0;
-    for cur_depth in 0..firewall.len() {
-        // Check if we got caught
-        if let Some(ref layer) = firewall[cur_depth] {
-            if layer.scanner_pos == 0 {
-                severity += cur_depth * (layer.range as usize);
-            }
-        }
-        // Advance scanners
-        for maybe_layer in &mut firewall {
-            if let Some(ref mut layer) = maybe_layer {
-                advance_scanner(layer);
-            }
-        }
+    let detection_severity_when_starting_immedialetly =
+        simulate_packet_travel(&firewall).unwrap_or(0);
+    println!(
+        "Detection severity when immediately sending package: {}",
+        detection_severity_when_starting_immedialetly
+    );
+
+    // Determine smallest delay after which a package can be send undetected
+    let mut firewall_copy = firewall.clone();
+    let mut delay_in_picoseconds = 0;
+    while simulate_packet_travel(&firewall_copy).is_some() {
+        delay_in_picoseconds += 1;
+        advance_scanners(&mut firewall_copy);
     }
-    println!("{}", severity);
+    println!(
+        "Necessary minimal delay needed to send package undetected: {}",
+        delay_in_picoseconds
+    );
 }
 
+/// Calculates detection severity after simulating package travel through given firewall
+/// Returns None if package was not detected
+fn simulate_packet_travel(firewall: &[Option<Layer>]) -> Option<usize> {
+    let mut firewall_copy = firewall.to_owned();
+
+    let mut detection_severity = None;
+    for cur_depth in 0..firewall_copy.len() {
+        // Check if we got caught
+        if let Some(ref layer) = firewall_copy[cur_depth] {
+            if layer.scanner_pos == 0 {
+                detection_severity =
+                    Some(detection_severity.unwrap_or(0) + cur_depth * (layer.range as usize))
+            }
+        }
+        advance_scanners(&mut firewall_copy);
+    }
+    detection_severity
+}
+
+/// Advances all scanners in every layer of a given firewall one time-step forward
+fn advance_scanners(firewall: &mut [Option<Layer>]) {
+    for maybe_layer in firewall {
+        if let Some(ref mut layer) = maybe_layer {
+            advance_scanner(layer);
+        }
+    }
+}
+
+/// Advances a scanner in a given firewall layer one time-step forward
 fn advance_scanner(layer: &mut Layer) {
     // Switch scanner direction if it is about to go out of bounds
     if (layer.scanner_pos + layer.scanner_velocity >= layer.range)
