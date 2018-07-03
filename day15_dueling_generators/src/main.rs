1755597d00aca8068714b90f37868c96d4bd91d0
Jakob Schwab <j.schwab@mail.de>
2018-07-03 17:19:18 +0200

    Adds day 15 part 2

--- day15_dueling_generators/src/main.rs
+++ day15_dueling_generators/src/main.rs
@@ -28,16 +28,36 @@ impl Iterator for Sequence {
 }
 
 fn main() {
+    // Part one
+    let sequence_length = 40_000_000;
     let sequence_a = Sequence::new(A_START, A_FACTOR);
     let sequence_b = Sequence::new(B_START, B_FACTOR);
 
-    let num_matching = sequence_a
-        .take(40_000_000)
-        .zip(sequence_b.take(40_000_000))
+    let num_matching_part_one = sequence_a
+        .take(sequence_length)
+        .zip(sequence_b.take(sequence_length))
         .filter(|(x, y)| lowest_16_bit_match(*x, *y))
         .count();
+    println!(
+        "Number of matching pairs for part one: {}",
+        num_matching_part_one
+    );
 
-    println!("{}", num_matching);
+    // Part two
+    let sequence_length = 5_000_000;
+    let sequence_a = Sequence::new(A_START, A_FACTOR);
+    let sequence_b = Sequence::new(B_START, B_FACTOR);
+
+    let num_matching_part_two = sequence_a
+        .filter(|x| x % 4 == 0)
+        .take(sequence_length)
+        .zip(sequence_b.filter(|x| x % 8 == 0).take(sequence_length))
+        .filter(|(x, y)| lowest_16_bit_match(*x, *y))
+        .count();
+    println!(
+        "Number of matching pairs for part two: {}",
+        num_matching_part_two
+    );
 }
 
 fn lowest_16_bit_match(x: u32, y: u32) -> bool {
