4d2b1b5d4c283b8d8d28885f7f43ec5d10b52360
Jakob Schwab <j.schwab@mail.de>
2018-07-03 15:02:34 +0200

    Adds day 14 part 2

--- day14_disk_fragmentation/src/main.rs
+++ day14_disk_fragmentation/src/main.rs
@@ -1,10 +1,13 @@
-fn main() {
-    let input_string = "stpzcrnm";
+const GRID_DIMENSIONS: usize = 128;
+const INPUT_STRING: &str = "stpzcrnm";
 
-    // Create grid
+fn main() {
+    // Create grid:
+    // - filled cells == 1
+    // - empty cells  == 0
     let mut grid = Vec::new();
-    for i in 0..128 {
-        let input_string = input_string.to_owned() + &format!("-{}", i);
+    for i in 0..GRID_DIMENSIONS {
+        let input_string = INPUT_STRING.to_owned() + &format!("-{}", i);
         let knot_hash = knot_hash(&input_string);
         let mut row = knot_hash_to_grid_row(&knot_hash);
         grid.append(&mut row);
@@ -12,7 +15,66 @@ fn main() {
 
     let num_filled_cells: u32 = grid.iter().sum();
     println!("Number of filled cells: {}", num_filled_cells);
+
+    // Count all regions by flood-filling each region
+    // NOTE: If a region is filled it cannot be filled again, as they are filled with two's
+    //       but only regions consisting one's are fillable.
+    let mut num_filled_regions = 0;
+    for y in 0..GRID_DIMENSIONS {
+        for x in 0..GRID_DIMENSIONS {
+            if flood_fill(&mut grid, x as i32, y as i32, 1, 2) > 0 {
+                num_filled_regions += 1;
+            }
+        }
+    }
+    println!("Number of filled regions: {}", num_filled_regions);
 }
+
+// Grid operations
+//--------------------------------------------------------------------------------------------------
+
+/// If the cell at given `x` and `y` coordinate matches `target_val` then the cell and its whole
+/// region will change its value to `new_val`.
+/// The number of cells that changed value is returned;
+fn flood_fill(grid: &mut [u32], x: i32, y: i32, target_val: u32, new_val: u32) -> usize {
+    if get_grid_val(grid, x, y) != target_val {
+        return 0;
+    }
+    set_grid_val(grid, x, y, new_val);
+    let mut num_filled_cells = 1;
+
+    num_filled_cells += flood_fill(grid, x + 1, y, target_val, new_val);
+    num_filled_cells += flood_fill(grid, x - 1, y, target_val, new_val);
+    num_filled_cells += flood_fill(grid, x, y + 1, target_val, new_val);
+    num_filled_cells += flood_fill(grid, x, y - 1, target_val, new_val);
+
+    num_filled_cells
+}
+
+fn get_grid_val(grid: &[u32], x: i32, y: i32) -> u32 {
+    if let Some(index) = validate_index(x, y) {
+        grid[index]
+    } else {
+        0
+    }
+}
+
+fn set_grid_val(grid: &mut [u32], x: i32, y: i32, val: u32) {
+    if let Some(index) = validate_index(x, y) {
+        grid[index] = val;
+    }
+}
+
+fn validate_index(x: i32, y: i32) -> Option<usize> {
+    if x < 0 || x >= GRID_DIMENSIONS as i32 || y < 0 || y >= GRID_DIMENSIONS as i32 {
+        None
+    } else {
+        Some(x as usize + y as usize * GRID_DIMENSIONS)
+    }
+}
+
+// Knot hash related
+//--------------------------------------------------------------------------------------------------
 
 fn knot_hash_to_grid_row(dense_hash: &[u8]) -> Vec<u32> {
     let mut result = Vec::new();
@@ -43,6 +105,9 @@ fn knot_hash(input_string: &str) -> Vec<u8> {
     }
     sparse_knot_hash_to_dense(&circular_list)
 }
+
+// Stuff from day 10
+//--------------------------------------------------------------------------------------------------
 
 fn sparse_knot_hash_to_dense(sparse_hash: &[u8]) -> Vec<u8> {
     sparse_hash
