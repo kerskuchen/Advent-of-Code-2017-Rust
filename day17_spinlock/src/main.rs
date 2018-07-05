4fc1f943cea6b6f58c45bf4804fbe7568c658bc9
Jakob Schwab <j.schwab@mail.de>
2018-07-05 20:36:24 +0200

    Adds day 17 part 2

--- day17_spinlock/src/main.rs
+++ day17_spinlock/src/main.rs
@@ -3,6 +3,8 @@ const STEP_SIZE: usize = 394;
 fn main() {
     let mut buffer = vec![0];
     let mut cur_pos = 0;
+
+    // Insert the first 2017 values
     for insert_val in 1..=2017 {
         let next_pos = (cur_pos + STEP_SIZE) % buffer.len();
         let insert_pos = next_pos + 1;
@@ -10,7 +12,29 @@ fn main() {
         cur_pos = insert_pos;
     }
     println!(
-        "The value after 2017 in the buffer is {}",
+        "After 2017 insertions the value following 2017 is {}",
         buffer[(cur_pos + 1) % buffer.len()]
+    );
+
+    // Insert the remaining values till 50,000,000 and check the value after the 0th index
+    //
+    // NOTE: Zero is always at the 0th index due to the way values are inserted into the buffer.
+    //       We can use this property to just skip all insertions that are not placed directly
+    //       behind the 0th index.
+    for insert_val in 2018..=50_000_000 {
+        let next_pos = (cur_pos + STEP_SIZE) % buffer.len();
+        let insert_pos = next_pos + 1;
+        if insert_pos == 1 {
+            // Only actually insert a value when it is a number directly following zero
+            buffer.insert(insert_pos, insert_val);
+        } else {
+            // We push back fake values to have a correct buffer length at every iteration
+            buffer.push(0);
+        }
+        cur_pos = insert_pos;
+    }
+    println!(
+        "After 50,000,000 insertions the value after 0 in the buffer is {}",
+        buffer[1]
     );
 }
