4591cceb94fb7b4c2dc9e3bde8666f1ffbc8cefd
Jakob Schwab <j.schwab@mail.de>
2018-07-07 13:11:56 +0200

    Adds day 18 part 2

--- day18_duet/src/main.rs
+++ day18_duet/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::*;
 
@@ -8,61 +9,138 @@ fn main() {
         .filter_map(|maybe_line| maybe_line.ok())
         .collect();
 
-    let mut cur_pos = 0;
-    let mut registers = HashMap::new();
-    let mut last_played_sound_frequency = None;
+    part_one(&lines);
+    part_two(&lines);
+}
+
+fn part_one(instructions: &[String]) {
+    let mut program = Program::new(ProgramType::PartOne);
+    program.excecute(instructions);
+    if let Some(frequency) = program.mailbox_out.pop_back() {
+        println!("Last recovered sound frequency: {}", frequency);
+    }
+}
+
+fn part_two(instructions: &[String]) {
+    let mut program0 = Program::new(ProgramType::PartTwo(0));
+    let mut program1 = Program::new(ProgramType::PartTwo(1));
     loop {
-        let instruction: Vec<_> = lines[cur_pos].split_whitespace().collect();
-        let opcode = instruction[0];
-        match opcode {
-            "snd" => {
-                let sound_frequency = extract_value(instruction[1], &mut registers);
-                last_played_sound_frequency = Some(sound_frequency);
-            }
-            "set" => {
-                let register_name = String::from(instruction[1]);
-                let value = extract_value(instruction[2], &mut registers);
-                let mut register_value = registers.entry(register_name).or_insert(0);
-                *register_value = value;
-            }
-            "add" => {
-                let register_name = String::from(instruction[1]);
-                let value = extract_value(instruction[2], &mut registers);
-                let mut register_value = registers.entry(register_name).or_insert(0);
-                *register_value += value;
-            }
-            "mul" => {
-                let register_name = String::from(instruction[1]);
-                let value = extract_value(instruction[2], &mut registers);
-                let mut register_value = registers.entry(register_name).or_insert(0);
-                *register_value *= value;
-            }
-            "mod" => {
-                let register_name = String::from(instruction[1]);
-                let value = extract_value(instruction[2], &mut registers);
-                let mut register_value = registers.entry(register_name).or_insert(0);
-                *register_value %= value;
-            }
-            "rcv" => {
-                let conditional = extract_value(instruction[1], &mut registers);
-                if conditional != 0 {
-                    if let Some(frequency) = last_played_sound_frequency {
-                        println!("Last recovered sound frequency: {}", frequency);
-                        break;
+        program0.excecute(instructions);
+        program1.excecute(instructions);
+        program0.mailbox_in.append(&mut program1.mailbox_out);
+        program1.mailbox_in.append(&mut program0.mailbox_out);
+
+        if program0.mailbox_in.is_empty() && program1.mailbox_in.is_empty() {
+            break;
+        }
+    }
+    println!("Program 0 sent {} messages", program0.num_messages_sent);
+    println!("Program 1 sent {} messages", program1.num_messages_sent);
+}
+
+enum ProgramType {
+    PartOne,
+    PartTwo(i64),
+}
+
+struct Program {
+    program_type: ProgramType,
+    mailbox_in: VecDeque<i64>,
+    mailbox_out: VecDeque<i64>,
+    registers: HashMap<String, i64>,
+    cur_instruction: usize,
+    num_messages_sent: usize,
+}
+
+impl Program {
+    fn new(program_type: ProgramType) -> Program {
+        let mut result = Program {
+            program_type,
+            mailbox_in: VecDeque::new(),
+            mailbox_out: VecDeque::new(),
+            registers: HashMap::new(),
+            cur_instruction: 0,
+            num_messages_sent: 0,
+        };
+        if let ProgramType::PartTwo(program_id) = result.program_type {
+            result.registers.insert(String::from("p"), program_id);
+        }
+        result
+    }
+
+    /// Part one: Executes given instructions until first "rcv" instruction is encountered
+    ///           and its execution condition is satisfied
+    /// Part two: Executes given instructions until a "rcv" instruction is
+    ///           encountered with empty inbox
+    fn excecute(&mut self, instructions: &[String]) {
+        loop {
+            let instruction: Vec<_> = instructions[self.cur_instruction]
+                .split_whitespace()
+                .collect();
+            let opcode = instruction[0];
+            match opcode {
+                "snd" => {
+                    let value = extract_value(instruction[1], &mut self.registers);
+                    self.mailbox_out.push_back(value);
+                    self.num_messages_sent += 1;
+                }
+                "set" => {
+                    let register_name = String::from(instruction[1]);
+                    let value = extract_value(instruction[2], &mut self.registers);
+                    let mut register_value = self.registers.entry(register_name).or_insert(0);
+                    *register_value = value;
+                }
+                "add" => {
+                    let register_name = String::from(instruction[1]);
+                    let value = extract_value(instruction[2], &mut self.registers);
+                    let mut register_value = self.registers.entry(register_name).or_insert(0);
+                    *register_value += value;
+                }
+                "mul" => {
+                    let register_name = String::from(instruction[1]);
+                    let value = extract_value(instruction[2], &mut self.registers);
+                    let mut register_value = self.registers.entry(register_name).or_insert(0);
+                    *register_value *= value;
+                }
+                "mod" => {
+                    let register_name = String::from(instruction[1]);
+                    let value = extract_value(instruction[2], &mut self.registers);
+                    let mut register_value = self.registers.entry(register_name).or_insert(0);
+                    *register_value %= value;
+                }
+                "rcv" => {
+                    // NOTE: Only the receive instruction is different in each problem part
+                    match self.program_type {
+                        ProgramType::PartOne => {
+                            let conditional = extract_value(instruction[1], &mut self.registers);
+                            if conditional != 0 {
+                                break;
+                            }
+                        }
+                        ProgramType::PartTwo(_) => {
+                            if let Some(value) = self.mailbox_in.pop_front() {
+                                let register_name = String::from(instruction[1]);
+                                let mut register_value =
+                                    self.registers.entry(register_name).or_insert(0);
+                                *register_value = value;
+                            } else {
+                                break;
+                            }
+                        }
                     }
                 }
-            }
-            "jgz" => {
-                let conditional = extract_value(instruction[1], &mut registers);
-                if conditional > 0 {
-                    let jump_offset = extract_value(instruction[2], &mut registers);
-                    cur_pos = (cur_pos as i64 + jump_offset) as usize;
-                    continue;
+                "jgz" => {
+                    let conditional = extract_value(instruction[1], &mut self.registers);
+                    if conditional > 0 {
+                        let jump_offset = extract_value(instruction[2], &mut self.registers);
+                        self.cur_instruction = (self.cur_instruction as i64 + jump_offset) as usize;
+                        continue;
+                    }
                 }
+                _ => panic!("Not an instruction: {}", opcode),
             }
-            _ => panic!("Not an instruction: {}", opcode),
+            self.cur_instruction += 1;
         }
-        cur_pos += 1;
     }
 }
 
