b8245f6139cb9067ef5d81c1bb3b37f00b60d58b
Jakob Schwab <j.schwab@mail.de>
2018-07-07 21:47:34 +0200

    Adds day 20 part 2

--- day20_particle_swarm/src/main.rs
+++ day20_particle_swarm/src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate text_io;
 
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::*;
 use std::ops::AddAssign;
 
 fn main() {
-    let mut particles: Vec<Particle> = BufReader::new(File::open("input.txt").unwrap())
+    let particles: Vec<Particle> = BufReader::new(File::open("input.txt").unwrap())
         .lines()
         .filter_map(|maybe_line| maybe_line.ok())
         .filter(|line| !line.is_empty())
@@ -14,6 +15,13 @@ fn main() {
         .map(|(index, line)| Particle::from_str(index, &line).unwrap())
         .collect();
 
+    part_one(particles.clone());
+    part_two(particles);
+}
+
+fn part_one(mut particles: Vec<Particle>) {
+    // We simulate particles and sort them by distance to origin, until the closest particle
+    // stays the same for 10_000 iterations
     let mut closest_particle_id = 0;
     let mut num_simulation_steps_without_change = 0;
     loop {
@@ -37,6 +45,66 @@ fn main() {
         "The particle which will stay closest to position <0,0,0> is particle {}",
         particles[0].id
     );
+}
+
+fn part_two(mut particles: Vec<Particle>) {
+    // We simulate and remove collided particles until the number of particles stays the same
+    // for 10_000 iterations
+    let mut num_simulation_steps_without_change = 0;
+    loop {
+        for particle in &mut particles {
+            particle.vel += particle.acc;
+            particle.pos += particle.vel;
+        }
+
+        let (collision_happened, particles_without_collision) = remove_collided(particles);
+        particles = particles_without_collision;
+        if collision_happened {
+            num_simulation_steps_without_change = 0;
+        }
+
+        num_simulation_steps_without_change += 1;
+        if num_simulation_steps_without_change > 10_000 {
+            break;
+        }
+    }
+    println!(
+        "The particle number of particles left after all collisions are resolved is {}",
+        particles.len()
+    );
+}
+
+/// Removes all colliding particles form a given particle vector by returning a
+/// new vector without them. Returns true if collisions have happened.
+fn remove_collided(mut particles: Vec<Particle>) -> (bool, Vec<Particle>) {
+    // Collect all positions where collisions are happening
+    // NOTE: Through sorting by position, we achieve that particle collision groups are consecutive
+    //       in the vector
+    particles.sort_by(|a, b| a.pos.cmp(&b.pos));
+    let mut collision_points = HashSet::new();
+    for possible_collision_point in particles.iter().map(|particle| particle.pos) {
+        if particles
+            .iter()
+            .filter(|particle| particle.pos == possible_collision_point)
+            .count() > 1
+        {
+            collision_points.insert(possible_collision_point);
+        }
+    }
+    if collision_points.is_empty() {
+        return (false, particles);
+    }
+
+    // Remove all particles that are located at collision points
+    let particles_without_collision = particles
+        .into_iter()
+        .filter(|particle| {
+            collision_points
+                .iter()
+                .all(|&col_point| particle.pos != col_point)
+        })
+        .collect();
+    (true, particles_without_collision)
 }
 
 #[derive(Clone, Copy)]
@@ -69,7 +137,7 @@ impl Particle {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, Ord, PartialOrd, PartialEq, Hash)]
 struct Vec3 {
     x: i64,
     y: i64,
