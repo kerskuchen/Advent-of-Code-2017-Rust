f88957673f65a6918b6bb7e3d0c0695f1d3ceb6b
Jakob Schwab <j.schwab@mail.de>
2018-07-11 07:31:44 +0200

    Adds day 23 part 2

--- day23_coprocessor_conflagration/src/main.rs
+++ day23_coprocessor_conflagration/src/main.rs
@@ -3,6 +3,11 @@ use std::fs::File;
 use std::io::*;
 
 fn main() {
+    part_one();
+    part_two();
+}
+
+fn part_one() {
     let instructions: Vec<String> = BufReader::new(File::open("input.txt").unwrap())
         .lines()
         .filter_map(|maybe_line| maybe_line.ok())
@@ -14,6 +19,205 @@ fn main() {
         "Num of mul instructions invoked: {}",
         program.num_mul_instructions_invoked
     );
+}
+
+fn part_two() {
+    // Original
+    // ----------------
+    //
+    // set b 79
+    // set c b
+    // jnz a 2 'a
+    // jnz 1 5 'b
+    // 'a: mul b 100
+    // sub b -100000
+    // set c b
+    // sub c -17000
+    // 'b: set f 1
+    // set d 2
+    // 'e set e 2
+    // 'd: set g d
+    // mul g e
+    // sub g b
+    // jnz g 2 'c
+    // set f 0
+    // 'c: sub e -1
+    // set g e
+    // sub g b
+    // jnz g -8 'd
+    // sub d -1
+    // set g d
+    // sub g b
+    // jnz g -13 'e
+    // jnz f 2 'f
+    // sub h -1
+    // 'f: set g b
+    // sub g c
+    // jnz g 2 'g
+    // jnz 1 3 'exit
+    // 'g sub b -17
+    // jnz 1 -23 'b
+
+    // if's, variable substitution and jump labels
+    // ----------------
+    //
+    // b = 79 * 100 + 100_000;
+    // c = b + 17_000;
+    // 'b: f = 1;
+    // d = 2;
+    // 'e e = 2;
+    // 'd: g = d
+    // g = g * e - b;
+    // if g == 0{
+    //     f = 0;
+    // }
+    // e += 1;
+    // g = e - b;
+    // jnz g -8 'd
+    // d+= 1;
+    // g = d-b;
+    // jnz g -13 'e
+    // if f==0{
+    //     h+=1;
+    // }
+    // g =b -c;
+    // if g == 0{
+    // jnz 1 3 'exit
+    // }
+    // 'g b += 17;
+    // jnz 1 -23 'b
+
+    // more variable substitution and loops
+    // ----------------
+    //
+    // b = 107_900;
+    // c = 124_900;
+    // 'main: loop {
+    //     f = 1;
+    //     d = 2;
+    //     do {
+    //         e = 2;
+    //         do
+    //         {
+    //             g = d * e - b;
+    //
+    //             if g == 0 {
+    //                 f = 0;
+    //             }
+    //
+    //             e += 1;
+    //             g = e - b;
+    //
+    //         } while g != 0
+    //
+    //         d += 1;
+    //         g = d - b;
+    //
+    //     } while g != 0
+    //
+    //     if f == 0 {
+    //         h += 1;
+    //     }
+    //
+    //     g = b - c;
+    //
+    //     if g == 0 {
+    //         break 'main;
+    //     }
+    //
+    //     b += 17;
+    // }
+
+    // Getting rid of g
+    // ----------------
+    //
+    // b = 107_900;
+    // c = 124_900;
+    // 'main: loop {
+    //     f = 1;
+    //     d = 2;
+    //     do {
+    //         e = 2;
+    //         do
+    //         {
+    //             if d * e == b {
+    //                 f = 0;
+    //             }
+    //
+    //             e += 1;
+    //         } while e != b
+    //
+    //         d += 1;
+    //     } while d != b
+    //
+    //     if f == 0 {
+    //         h += 1;
+    //     }
+    //
+    //     if b == c {
+    //         break 'main;
+    //     }
+    //
+    //     b += 17;
+    // }
+
+    // even more variable substitution and for loops
+    // ----------------
+    //
+    // b = 107_900;
+    // c = 124_900;
+    // while b <= b {
+    //     f = 1;
+    //     for d in 2..b {
+    //         for e in 2..b {
+    //             if d * e == b {
+    //                 f = 0;
+    //             }
+    //         }
+    //     }
+    //     if f == 0 {
+    //         h += 1;
+    //     }
+    //     b += 17;
+    // }
+
+    // Better semantics
+    // ----------------
+    // let mut h = 0;
+    // let mut b = 107_900;
+    // let c = 124_900;
+    // while b <= b {
+    //     // We increment h if b can be represented as a product of exactly two factors d,e
+    //     // with d,e in [2,b[
+    //     'inner: for d in 2..b {
+    //         for e in 2..b {
+    //             if d * e == b {
+    //                 h += 1;
+    //                 break 'inner;
+    //             }
+    //         }
+    //     }
+    //     b += 17;
+    // }
+
+    // Optimization and better semantics
+    // ----------------
+    //
+    let mut h = 0;
+    let mut b = 107_900;
+    let c = 124_900;
+    while b <= c {
+        // We increment h if b is not prime
+        'inner: for d in 2..b {
+            if b % d == 0 {
+                h += 1;
+                break 'inner;
+            }
+        }
+        b += 17;
+    }
+
+    println!("Value in register 'h' after program has finished: {}", h);
 }
 
 struct Program {
